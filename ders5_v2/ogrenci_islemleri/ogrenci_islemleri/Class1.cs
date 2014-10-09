a89d891d1f850345bd8d1830910c0e091c8e7238
ozgehalilduman <ozgehalilduman@gmail.com>
2014-10-10 00:13:28 +0300

    v0.2
    Ders 5 örneğinin gelişmişi
    --EKLENEN ÖZELLİKLER
    -Belirli bir sayıda öğrenci ekleme
    -belirli bir sıradaki öğrenciyi listeleme
    --İŞLENEN KONULAR
    *parametreli metot kullanımı
    *metotları OVERLOAD etme

---
+++ ders5_v2/ogrenci_islemleri/ogrenci_islemleri/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenci_islemleri
+{
+    /// <summary>
+    /// ogrencilerin temel propertylerinin-özelliklerinin tanımlandığı kısım
+    /// </summary>
+    public class ogrenci
+    {
+        public string ad;
+        public string soyad;
+        public string sinif;
+        public string okulno;
+        public char cinsiyet;
+        public byte boy;
+        public byte kilo;
+    }
+    /// <summary>
+    /// ogrencilerle ilgili işlemlerin eylemlerin tanımlandığı kısım...
+    /// </summary>
+    public class ogrenci_islemler
+    {
+        List<ogrenci> ogr_lst = new List<ogrenci>();
+        /// <summary>
+        /// yeni ogrenci ekleme metodu-eylemi
+        /// </summary>
+        public void ogrenci_ekle()
+        {
+            ogrenci ogr = new ogrenci();
+            Console.Write("\tAdınızı Giriniz\t\t\t: ");
+            ogr.ad = Console.ReadLine();
+            Console.Write("\tSoyadınızı Giriniz\t\t: ");
+            ogr.soyad = Console.ReadLine();
+            Console.Write("\tSınıfınızı Giriniz\t\t: ");
+            ogr.sinif = Console.ReadLine();
+            Console.Write("\tOkul Numaranızı Giriniz\t\t: ");
+            ogr.okulno = Console.ReadLine();
+            Console.Write("\tCinsiyetinizi Giriniz(E/B)\t: ");
+            ogr.cinsiyet =Convert.ToChar(Console.ReadLine());
+            Console.Write("\tBoyunuzu Giriniz\t\t: ");
+            ogr.boy = Convert.ToByte(Console.ReadLine());
+            Console.Write("\tKilonuzu Giriniz\t\t: ");
+            ogr.kilo = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("-----------------------------");
+            //girilen ogrenciyi listeye ekliyorum..
+            ogr_lst.Add(ogr);
+        }
+        /// <summary>
+        /// Belirtilen adet yeni ogrenci ekleme metodu-eylemi
+        /// </summary>
+        /// <param name="adet">KAç adet Öğrenci Ekleneceğini Belirliyorum</param>
+        public void ogrenci_ekle(int adet)
+        {
+            for (int s = 0; s < adet; s++)
+            {
+                ogrenci_ekle();
+            }
+           //Bu metot ile hem parametre kullanıma hemde OVERLOAD a ornek vermiş olduk      
+        }
+        /// <summary>
+        /// Mevcut Kayıtlı Öğrencileri listeme komutu
+        /// </summary>
+        public void ogrenci_listele()
+        {
+            int sayac = 0;
+            foreach(ogrenci _ogr in ogr_lst)
+            {
+                sayac++;
+                Console.WriteLine("{0}. ÖGRENCİ KAYDI\n---------------------",sayac);
+                Console.WriteLine("AD-SOYAD\t: {0} {1}",_ogr.ad,_ogr.soyad);
+                Console.WriteLine("SINIFI\t\t: {0}", _ogr.sinif);
+                Console.WriteLine("OKULNO\t\t: {0}", _ogr.okulno);
+                Console.WriteLine("CİNSİYETİ\t: {0}", _ogr.cinsiyet=='e'?"BAY":"BAYAN");
+                Console.WriteLine("BOYU\t\t: {0}",_ogr.boy);
+                Console.WriteLine("KİLO\t\t: {0}", _ogr.kilo);
+                Console.WriteLine("OBEZİTE DURUM\t: {0}", obezite_kontrol(_ogr.kilo,_ogr.boy));
+            }
+            if (sayac == 0) 
+            {
+                Console.WriteLine("==== KAYITLI OGRENCİ YOK ====");
+            }
+        }
+        /// <summary>
+        /// Belirli sıradaki ogrenciyi ekrana yazan metot
+        /// </summary>
+        /// <param name="kacinci"></param>
+        public void ogrenci_listele(int kacinci)
+        {
+            Console.WriteLine("{0}. ÖGRENCİ KAYDI\n---------------------", kacinci);
+            try { 
+                ogrenci _ogr = ogr_lst[kacinci-1];
+
+                Console.WriteLine("AD-SOYAD\t: {0} {1}", _ogr.ad, _ogr.soyad);
+                Console.WriteLine("SINIFI\t\t: {0}", _ogr.sinif);
+                Console.WriteLine("OKULNO\t\t: {0}", _ogr.okulno);
+                Console.WriteLine("CİNSİYETİ\t: {0}", _ogr.cinsiyet == 'e' ? "BAY" : "BAYAN");
+                Console.WriteLine("BOYU\t\t: {0}", _ogr.boy);
+                Console.WriteLine("KİLO\t\t: {0}", _ogr.kilo);
+                Console.WriteLine("OBEZİTE DURUM\t: {0}", obezite_kontrol(_ogr.kilo, _ogr.boy));
+            }
+            catch
+            {
+                Console.WriteLine("==== KAYITLI OGRENCİ YOK ====");
+            }
+        }
+        /// <summary>
+        /// ozbezite indeksine göre 2.5 girilen boy ve kilo değerlerine göre obezite durumunu belirler
+        /// </summary>
+        /// <param name="kilo">Kişinin Kilo bilgisi</param>
+        /// <param name="boy">Kişinin boy bilgisi</param>
+        /// <returns></returns>
+        public string obezite_kontrol(byte kilo,byte boy)
+        { 
+            double endeks=(double)boy/(double)kilo;
+            return endeks < 2.5 ? "NORMAL" : "OBEZ";
+        }
+    }
+
+}
---
+++ ders5_v2/ogrenci_islemleri_consoleapp/ogrenci_islemleri_consoleapp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ogrenci_islemleri;
+
+namespace ogrenci_islemleri_consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci_islemler ogr = new ogrenci_islemler();
+            ogr.ogrenci_ekle(5);
+            ogr.ogrenci_listele();
+            ogr.ogrenci_listele(3);
+            ogr.ogrenci_listele(7);
+            Console.ReadKey();            
+        }
+    }
+}
