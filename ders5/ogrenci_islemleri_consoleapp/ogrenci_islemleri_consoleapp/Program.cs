9eab8812167f1caa1abe21752a9bd6942a20b0ea
ozgehalilduman <ozgehalilduman@gmail.com>
2014-10-09 23:41:34 +0300

    v0.1
    Fazla karışmasın diyerek try catch kullanmadım.

---
+++ ders5/ogrenci_islemleri_consoleapp/ogrenci_islemleri_consoleapp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ogrenci_islemleri;
+
+namespace ogrenci_islemleri_consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci_islemler ogr = new ogrenci_islemler();
+            ogr.ogrenci_ekle();
+            ogr.ogrenci_ekle();
+            ogr.ogrenci_listele();
+            Console.ReadKey();            
+        }
+    }
+}
